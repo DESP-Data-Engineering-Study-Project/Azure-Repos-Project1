9fefe83580228a8712f9f360a3632a2961e02ad9
DESubha <109506480+DESubha@users.noreply.github.com>
2022-09-03 13:56:55 +0530

    Changeset 105.2 AB#105 Azure SQL Table Script
    Table of testing Details data ingested from datalake

---
+++ AzureSQL_TableScript-tabtesting.sql
@@ -0,0 +1,12 @@
+create table tabtesting
+(
+country   varchar(100),
+country_code varchar(100),
+year_week varchar(100),
+new_cases varchar(100),
+tests_done varchar(100),
+population varchar(100),
+testing_rate varchar(100),
+positivity_rate varchar(100),
+testing_data_source varchar(100)
+)
